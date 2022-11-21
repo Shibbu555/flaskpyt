f521e4163026f930377de2363229c1fee07ba200
Shibbu555 <118251808+Shibbu555@users.noreply.github.com>
2022-11-21 18:24:27 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,jsonify,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({'message':'welcome'})
+
+@app.route('/on')
+def on():
+    return jsonify({'state':'1'})
+
+@app.route('/off')
+def off():
+    return jsonify({'state':'0'})
+
+if _name=='__main__':
+	app.run(debug=True)
